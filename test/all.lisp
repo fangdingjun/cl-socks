d973dc243aa3a887da6d2e84fb25e34c43e48a3a
dingjun <fangdingjun@gmail.com>
2022-03-08 15:57:23 +0800

    clean up after test

--- test/all.lisp
+++ test/all.lisp
@@ -9,25 +9,34 @@
 (in-suite all-tests)
 
 (test test-socks
-  (let* ((srv (socketserver:create-server "0.0.0.0" 0 #'socketserver:handler))
-         (sock (socks:listen-for-connection "0.0.0.0" 0))
-         (s (socks:connect "127.0.0.1"
-                           (usocket:get-local-port sock)
-                           "debian"
-                           (usocket:get-local-port srv)))
-         (s1 (usocket:socket-stream s)))
-    (format t "socks port ~a~%" (usocket:get-local-port sock))
-    (format t "server port ~a~%" (usocket:get-local-port srv))
-    (unwind-protect
-     (progn
-      (socks:write-bytes-n s1 '(#x1 #x2 #x3 #x21 #x48 #x90))
-      (is (= (nth 5 (socks:read-bytes-n s1 6)) #x90)))
+  (multiple-value-bind (t1 srv)
+      (socketserver:create-server "0.0.0.0" 3458 #'socketserver:handler)
+    (multiple-value-bind (t2 sock)
+        (socks:listen-for-connection "0.0.0.0" 3459)
+      (let ((s)
+            (s1))
+        (setf s (socks:connect "127.0.0.1"
+                               (usocket:get-local-port sock)
+                               "debian"
+                               (usocket:get-local-port srv)))
 
-     (progn
-      (usocket:socket-close srv)
-      (usocket:socket-close s)
-      (usocket:socket-close sock)
-      (format t "close all sockets done~%")))))
+        (setf s1 (usocket:socket-stream s))
+
+        (format t "socks port ~a~%" (usocket:get-local-port sock))
+        (format t "server port ~a~%" (usocket:get-local-port srv))
+
+        (unwind-protect
+         (progn
+          (socks:write-bytes-n s1 '(#x1 #x2 #x3 #x21 #x48 #x90))
+          (is (= (nth 5 (socks:read-bytes-n s1 6)) #x90)))
+
+         (progn
+          (usocket:socket-close srv)
+          (usocket:socket-close s)
+          (usocket:socket-close sock)
+          (bt:destroy-thread t1)
+          (bt:destroy-thread t2)
+          (format t "close all sockets done~%")))))))
 
 (defun test-suite ()
   (run! 'all-tests)
